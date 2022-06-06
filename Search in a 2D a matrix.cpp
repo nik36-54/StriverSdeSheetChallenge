db7d3b8fa0e1dbd19ff67b8a34c19cdf9a6e2056
Niraj kumar <81903058+nik36-54@users.noreply.github.com>
2022-06-06 17:10:54 +0530

    06-06-2022

--- Search in a 2D a matrix.cpp
+++ Search in a 2D a matrix.cpp
@@ -13,3 +13,26 @@ bool findTargetInMatrix(vector<vector<int>>& mat,int m,int n,int target) {
 }
 
 // O(m+n) time O(1) space
+
+*************************************************
+// But there is also a O(log(m*n)) time approach.
+
+bool findTargetInMatrix(vector<vector<int>> & mat, int m, int n, int target) {
+    int start = 0, end = m * n - 1;
+
+    // Binary search.
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        int val = mat[mid / n][mid % n];
+
+        if (target < val) {
+            end = mid - 1;
+        } else if (target > val) {
+            start = mid + 1;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
