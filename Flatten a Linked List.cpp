e167dae4a5342717695a1f3cca04b3b7557e12bb
Niraj kumar <81903058+nik36-54@users.noreply.github.com>
2022-06-28 16:52:48 +0530

    28-06-2022

---
+++ Flatten a Linked List.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+class Node {
+public:
+	int data;
+	Node* next;
+	Node* child;
+
+	Node(int data) {
+		this->data = data;
+		this->next = NULL;
+		this->child = NULL;
+	}
+};
+
+*****************************************************************/
+
+Node* merge(Node* a,Node* b)
+{
+    if(a==NULL)return b;
+    if(b==NULL)return a;
+    Node* ans;
+    if(a->data<b->data)
+    {
+        ans=a;
+        ans->child=merge(a->child,b);
+    }
+    else
+    {
+        ans=b;
+        ans->child=merge(a,b->child);
+    }
+    ans->next=NULL;
+    return ans;
+}
+
+
+Node* flattenLinkedList(Node* head) 
+{
+	if(head==NULL || head->next==NULL)return head;
+    
+    head->next=flattenLinkedList(head->next);
+    head=merge(head,head->next);
+    
+    return head;
+}
