6db7290b62927b2b1e62baacc10aaa3de8c12d53
Niraj kumar <81903058+nik36-54@users.noreply.github.com>
2022-07-12 22:46:35 +0530

    12-07-2022

---
+++ 01 Knapsack.cpp
@@ -0,0 +1,21 @@
+int maxProfit(vector<int> &val, vector<int> &wt, int n, int W)
+{
+	int i, w;
+      vector<vector<int>> K(n + 1, vector<int>(W + 1));
+ 
+    for(i = 0; i <= n; i++)
+    {
+        for(w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] +
+                                K[i - 1][w - wt[i - 1]],
+                                K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
+}
