dba0185026d6106464f4fcad14f81e34a22a7ea8
Niraj kumar <81903058+nik36-54@users.noreply.github.com>
2022-07-12 22:21:41 +0530

    12-07-2022

---
+++ Edit Distance.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+int editDistance(string s1, string s2)
+{
+    int n=s1.length(),m=s2.length();
+    int dp[n+1][m+1]={0};
+    
+    for(int i=0;i<=n;i++)
+        dp[i][0]=i;
+    for(int i=0;i<=m;i++)
+        dp[0][i]=i;
+    dp[0][0]=0;
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(s1[i-1]==s2[j-1])
+                dp[i][j]=dp[i-1][j-1];
+            else
+                dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+        }
+    }
+    return dp[n][m];
+}
