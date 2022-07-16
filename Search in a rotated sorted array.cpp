6bdf28b8d9428165e94ff8024be1f6e9d8bcd4d0
Niraj kumar <81903058+nik36-54@users.noreply.github.com>
2022-07-16 16:29:38 +0530

    16-07-2022

---
+++ Search in a rotated sorted array.cpp
@@ -0,0 +1,27 @@
+int search(int* arr, int key) {
+        int n=arr.size();
+        int low=0,high=n-1;
+        
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]==key)
+            return mid;
+            
+            if(arr[low]<=arr[mid])
+            {
+                if(arr[low]<=key && key<=arr[mid])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            else
+            {
+                if(arr[mid]<=key && key<=arr[high])
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return -1;
+    }
